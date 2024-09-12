4c2a8e05946075937f2aed2be844f28eef49d6eb
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-12 05:16:26 -0700

    working

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,25 @@
 import { Box, Typography } from "@mui/material"
-
+import "../pages/About.css"
 
 function About() {
 
   return (
     <>
-    <Box className = 'container'>
-        <Typography variant="h2">About</Typography>
+    <Box>
+      <Typography className='text-center mt-5' variant='h5'>About Us</Typography>
+    </Box>
+    <Box className = 'container mb-5 mt-5 about-main'>
+      <Box className ="img-box">
+        <img className="img" src="https://e-commerce-website1-lac.vercel.app/assets/about_img-BAJyTXw9.png" alt="" />
+      </Box>
+      <Box className ="text-box">
+      <Typography  className ="text-1" variant="p">Forever was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.</Typography>
+      <Typography  className ="text-2" variant="p">Since our inception, we have worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty to electronics and home essentials, we offer an extensive collection sourced from trusted brands and suppliers.</Typography>
+      <Typography  className ="text-3" variant="h6">Our Mission</Typography>
+      <Typography  className ="text-4" variant="p">Our mission at Forever is to empower customers with choice, convenience, and confidence. We have dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</Typography>
+
+      </Box>
+        
     </Box>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Card from '../components/Card';
 import HeroSection from "../components/HeroSection"
 import Footer from '../components/Footer';
 import Detail from '../components/Detail';
+import About from './About';
 
 
 
@@ -22,6 +23,7 @@ function Home() {
       <Typography className='text-center p-2' variant='h5'>Services Provide</Typography>
     </Box>
     <Detail/>
+    <About />
     <Footer/>
     </>
   )
